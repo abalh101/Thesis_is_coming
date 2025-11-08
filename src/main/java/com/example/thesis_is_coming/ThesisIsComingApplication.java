eb2705c5d42eae6959f8541fd65569c085c95020
abalh101 <abdmalla1996@gmail.com>
2025-11-08 19:36:21 +0100

    initial commit

---
+++ src/main/java/com/example/thesis_is_coming/ThesisIsComingApplication.java
@@ -0,0 +1,14 @@
+package com.example.thesis_is_coming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisIsComingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThesisIsComingApplication.class, args);
+        System.out.println("The application is working");
+    }
+
+}
---
+++ src/test/java/com/example/thesis_is_coming/ThesisIsComingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thesis_is_coming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisIsComingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
